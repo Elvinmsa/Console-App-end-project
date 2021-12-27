dbc6b743301deb04dad580357bec28567b1d1a4e
Elvinmsa <Elvinmsa@github.com>
2021-12-26 17:07:24 -0800

    Console Application end project

---
+++ ConsoleAppEndProject/ConsoleAppEndProject/Program.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Models;
+using Service.Helpers;
+using Service.Services;
+using Service.Services.Interfaces;
+using System;
+
+namespace ConsoleAppEndProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CompanyService companyService = new CompanyService();
+
+            Helper.WriteToConsole(ConsoleColor.Blue, "Select options");
+
+            while (true)
+            {
+                GetMenus();
+
+                Console.ReadLine();
+
+                Helper.WriteToConsole(ConsoleColor.Cyan, "Add company name: ");
+                string companyName = Console.ReadLine();
+                Helper.WriteToConsole(ConsoleColor.Cyan, "Add company address: ");
+                string companyAddress = Console.ReadLine();
+
+                Company company = new Company
+                {
+                    Name = companyName,
+                    Address = companyAddress
+                };
+                companyService.Create(company);
+
+                Helper.WriteToConsole(ConsoleColor.Green, $"{company.Id} - {company.Name} Company created!");
+            }
+        }
+        private static void GetMenus()
+        {
+            Helper.WriteToConsole(ConsoleColor.Cyan, "1 - Create Company, 2 - Update Company, 3 - Delete Company, 4 - Get Company by id" +
+                "5 - Get all Company by name, 6 - Get all Company, 7 - Create Employee, 8 - Update Employee, 9 - Delete Employee" +
+                "10 - Get Employee by id, 11 - Get Employee by age, 12 - Get all Employee by Compnay id");
+        }
+    }
+}
---
+++ ConsoleAppEndProject/Service/Services/CompanyService.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Models;
+using Repository.Implementations;
+using Service.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Services
+{
+    public class CompanyService : ICompanyService
+    {
+        public CompanyRepository _companyRepository;
+        private int Count { get; set; }
+        public CompanyService()
+        {
+            _companyRepository = new CompanyRepository();
+        }
+        public Company Create(Company model)
+        {
+            model.Id = Count;
+            _companyRepository.Create(model);
+            Count++;
+            return model;
+        }
+
+        public void Delete(Company model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Company Get(Predicate<Company> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Company> GetAll(Predicate<Company> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Company Update(int id, Company model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
